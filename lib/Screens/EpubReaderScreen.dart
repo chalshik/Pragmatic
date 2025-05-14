ae80cb15d2eb2a9142121931bf9f1a719578a050
Ijgilik <zakupaku34@gmail.com>
2025-05-14 20:17:31 +0600

    Blah blah

--- lib/Screens/EpubReaderScreen.dart
+++ lib/Screens/EpubReaderScreen.dart
@@ -28,8 +28,52 @@ class _EpubReaderScreenState extends State<EpubReaderScreen> {
                   onChaptersLoaded: (chapters) {},
                   onEpubLoaded: () async {},
                   onRelocated: (value) {},
-                  onTextSelected: (epubTextSelection) {},
-                  ),
+                  onTextSelected: (epubTextSelection) {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (context) => DraggableScrollableSheet(
+                        expand: false,
+                        initialChildSize: 0.3,
+                        minChildSize: 0.1,
+                        maxChildSize: 0.8,
+                        builder: (context, scrollController) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                            ),
+                            child: ListView(
+                              controller: scrollController,
+                              children: [
+                                Center(
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(vertical: 8),
+                                    height: 4,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[400],
+                                      borderRadius: BorderRadius.circular(2),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Text(
+                                    'Selected: ${epubTextSelection.selectedText}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                                // Add more dictionary or info widgets here
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
               ),
             ],
           )
